f7c132124472c76ea029e887bad71b132cb70810
kodemeister <kodemeister@outlook.com>
2022-12-06 04:30:48 +0600

    Make JavaScript test file

---
+++ test.js
@@ -0,0 +1,39 @@
+class Shape {
+  name = ""
+
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Rectangle extends Shape {
+  width = 0
+  height = 0
+
+  constructor(name, width, height) {
+    super(name);
+    this.width = width > 0 ? width : 0;
+    this.height = height > 0 ? height : 0;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
+}
+
+const createShape = (type, ...args) => {
+  switch (type) {
+    case "Rectangle":
+      return new Rectangle(...args);
+    default:
+      throw new Error("Invalid shape type");
+  }
+}
+
+const obj = {
+  id: 1,
+  shape: createShape("Rectangle", "foo", 3, 4)
+}
+
+console.log(`Shape name: ${obj.shape.name}`);
+console.log(`Shape area: ${obj.shape.area()}`);
